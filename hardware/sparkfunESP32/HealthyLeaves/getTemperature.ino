76c34a705d9b8cff6e9a332339ac409a18e32c1c
VicentiuCristianBadea <vily.badea@gmail.com>
2020-11-10 14:24:12 -0500

    Revert "Vily notifications"

--- hardware/sparkfunESP32/HealthyLeaves/getTemperature.ino
+++
@@ -1,9 +0,0 @@
-// getTemperature
-
-int getTemperature()
-{
-  // Convert analog reading to celsius and calibrate
-  tmpRaw = analogRead(tmp);
-  tmpCelsius = (((tmpRaw * (3.3 / 1024)) - 0.5) * 10) + 6.8;
-  return tmpCelsius;
-}
