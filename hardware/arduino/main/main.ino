181be695e1fbe71d78d7f6b42a718684ab0fdff2
dansavin <dansavin11@gmail.com>
2020-11-10 21:30:37 -0500

    added Arduino code for moisture sensor read (no Firebase)

---
+++ hardware/arduino/main/main.ino
@@ -0,0 +1,21 @@
+/****************************************************/
+//Name: Sensor Read
+//Author: dansavin11@gmail.com
+//Last modified: 2020-11-10
+//Description: Program reads moisture data from a
+//             capacitive sensor and translates it
+//             to sensible readings.
+/****************************************************/
+
+//variables declaration
+int moistureData = 0;//A0
+
+void setup()
+{
+  Serial.begin(9600);//start serial comm with pc
+}
+
+void loop()
+{
+  moistureData = getMoisture();
+}
