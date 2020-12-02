4211c7c5c3b66a0e18514539ef2270202ceaad00
vily <vily.badea@gmail.com>
2020-12-02 01:00:37 -0500

    cloud functions changes

--- firecast/functions/src/index.ts
+++ firecast/functions/src/index.ts
@@ -12,27 +12,125 @@ admin.initializeApp();
 //    response.send("Hello from Firebase!");
 //});
 
-exports.getTemperature = functions.database.ref('/Temperature/')
-    .onWrite(async (change, context) =>{
-        const original = change.after.val();
-        const old = change.before.val();
-        const vals = original - old;
+exports.getTemperature = functions.database.ref('/Temperature/{id}')
+    .onWrite((change: { after: any; }, context: any) =>{
+        
+        
+        const snapshot = change.after;
+        const val = snapshot.val();
+        const time = val.time;
+        const userPlantId = val.userPlantId;
+        const value = val.value;
+        
+        const stringTime = JSON.stringify(time);
+        const stringUserPlantId = JSON.stringify(userPlantId);
+        const stringValue = JSON.stringify(value);
+
     
-        console.log('New temperature value for plant: ', vals);
+        console.log('Posted data for values: ', val)
+
+    
+        console.log('New temperature value for plant: ', stringUserPlantId, ' with value ', stringValue, 'at time ', stringTime);
 
         const payload = {
-            notification: {
-                title: 'The temperature is too low!',
-                body: 'Please check on the plant ${userPlantId}.',
-            },
             topic: 'temps',
+            data:{
+                type: "temperature",
+                time: stringTime,
+                userPlantId: stringUserPlantId,
+                value: stringValue,
+            },
         };
         
-       admin.messaging().send(payload)
-       .then((response) => {
+       return admin.messaging().send(payload)
+       .then((response: any) => {
            console.log('Successfully sent message: ', response);
        })
-       .catch((error) => {
+       .catch((error: any) => {
            console.log('Error sending message: ', error);
        });
+
 })
+
+exports.getMoisture = functions.database.ref('/Moisture/{id}')
+    .onWrite((change: { after: any; }, context: any) =>{
+        
+        
+        const snapshot = change.after;
+        const val = snapshot.val();
+        const time = val.time;
+        const userPlantId = val.userPlantId;
+        const value = val.value;
+        
+        const stringTime = JSON.stringify(time);
+        const stringUserPlantId = JSON.stringify(userPlantId);
+        const stringValue = JSON.stringify(value);
+
+    
+        console.log('Posted data for values: ', val)
+
+    
+        console.log('New moisture value for plant: ', stringUserPlantId, ' with value ', stringValue, 'at time ', stringTime);
+
+        const payload = {
+            topic: 'temps',
+            data:{
+                type: "moisture",
+                time: stringTime,
+                userPlantId: stringUserPlantId,
+                value: stringValue,
+            },
+        };
+        
+       return admin.messaging().send(payload)
+       .then((response: any) => {
+           console.log('Successfully sent message: ', response);
+       })
+       .catch((error: any) => {
+           console.log('Error sending message: ', error);
+       });
+
+})
+
+
+exports.getLight = functions.database.ref('/Light/{id}')
+    .onWrite((change: { after: any; }, context: any) =>{
+        
+        
+        const snapshot = change.after;
+        const val = snapshot.val();
+        const time = val.time;
+        const userPlantId = val.userPlantId;
+        const value = val.value;
+        
+        const stringTime = JSON.stringify(time);
+        const stringUserPlantId = JSON.stringify(userPlantId);
+        const stringValue = JSON.stringify(value);
+
+    
+        console.log('Posted data for values: ', val)
+
+    
+        console.log('New light value for plant: ', stringUserPlantId, ' with value ', stringValue, 'at time ', stringTime);
+
+        const payload = {
+            topic: 'temps',
+            data:{
+                type: "light",
+                time: stringTime,
+                userPlantId: stringUserPlantId,
+                value: stringValue,
+            },
+        };
+        
+       return admin.messaging().send(payload)
+       .then((response: any) => {
+           console.log('Successfully sent message: ', response);
+       })
+       .catch((error: any) => {
+           console.log('Error sending message: ', error);
+       });
+
+})
+
+
